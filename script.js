bb647eb5277a8c7edb7dfcf39d77cc1520044d39
Andrew <animduba@gmail.com>
2023-06-16 01:58:54 -0500

    Se creo el archivo script.js para empezar con el codigo del encriptador

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var botonEncriptar = document.getElementById('botonEncriptar');
+var botonDesencriptar = document.getElementById('botonDesencriptar');
+
+function hola () {
+    alert('Hola');
+}
+
+botonEncriptar.addEventListener('click', hola);
+
